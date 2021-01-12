f8f2213d4a6e7de8f4c3c3eace8641daad6fa2ce
woehrer12 <Julian.woehrer@gmail.com>
2021-01-12 12:04:19 +0100

    Update GridTracker.sh

--- skripte/GridTracker.sh
+++ skripte/GridTracker.sh
@@ -2,13 +2,19 @@
 # Beschreibung: Downloaded GridTracker Version 1.20 und entpackt es. Die Downloaddatei wird automatisch wieder gelöscht.
 # Aufruf:       /skripte/GridTracker.sh
 # Autor:        woehrer
-# Version:      1
-# Datum:        2021-01-10
+# Version:      2
+# Datum:        2021-01-12
 
 #!/bin/bash
 
 echo =====GridTracker 1.20=====
 cd #Initialisiere
+
+if [ -e ./GridTracker/GridTracker ]  # existiert GridTracker?
+	then
+		echo =====GridTracker existiert bereits=====
+	else
+		echo =====GridTracker wird installiert=====		
 
 #name Der Download Datei
 datei=GridTracker-Linux-64-1.20.0927.tar.gz?dl=1 
@@ -20,7 +26,7 @@ echo =====GridTracker wird entpackt=====
 tar xfv $datei
 =====Download Datei wieder gelöscht=====
 rm $datei
-
+fi
 # Versiontest Version 1
 # Tested by: woehrer
 # Test date: 2021-01-10
