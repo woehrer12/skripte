8f7eb1b975569aed2edd05d39f4fad8dd0b890b4
woehrer12 <Julian.woehrer@gmail.com>
2021-01-17 12:21:01 +0100

    docker ps -> sudo docker ps

--- skripte/docker-install.sh
+++ skripte/docker-install.sh
@@ -48,7 +48,7 @@ echo =====Portainer installieren=====
 
 sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
 
-docker ps
+sudo docker ps
 
 # Versiontest Version 2
 # Tested by: woehrer
