ce594ac3e33187f81775edf8796d5b18851250ed
woehrer12 <Julian.woehrer@gmail.com>
2021-01-17 12:31:25 +0100

    Portainer auf Portainer 2 geändert

--- skripte/docker-install.sh
+++ skripte/docker-install.sh
@@ -46,7 +46,7 @@ sudo docker run hello-world
 
 echo =====Portainer installieren=====
 
-sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 sudo docker ps
 
