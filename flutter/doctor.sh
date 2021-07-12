a782ebe977004e8f42587742dae133531c7b359d
Julian Wöhrer <julian.woehrer@gmail.com>
2021-07-12 17:09:33 +0200

    Flutter doctor

---
+++ flutter/doctor.sh
@@ -0,0 +1,18 @@
+# Script Name:  Flutter doctor
+# Beschreibung: Prüft bei Flutter die Installation
+# Aufruf:       /skripte/flutter/doctor.sh
+# Autor:        woehrer
+# Version:      1
+# Datum:        2021-07-12
+
+#!/bin/bash
+
+echo =====Flutter Doctor=====
+#cd #Initialisiere
+
+flutter doctor
+
+# Versiontest Version 1
+# Tested by: woehrer
+# Test date: 2021-07-12
+# Test Systems: Ubuntu 20
---
+++ flutter/run.sh
@@ -0,0 +1,18 @@
+# Script Name:  Führt die App mit No-Sound-Null-Safety aus
+# Beschreibung: Führt die Flutter App aus
+# Aufruf:       /skripte/flutter/run.sh
+# Autor:        woehrer
+# Version:      1
+# Datum:        2021-07-12
+
+#!/bin/bash
+
+echo =====Flutter Doctor=====
+#cd #Initialisiere
+
+flutter run --no-sound-null-safety
+
+# Versiontest Version 1
+# Tested by: woehrer
+# Test date: 2021-07-12
+# Test Systems: Ubuntu 20
