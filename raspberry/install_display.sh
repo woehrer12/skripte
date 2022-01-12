92dfc45e11c990e32cfa2e0703c7c9faf69608ad
Julian Wöhrer <julian.woehrer@gmail.com>
2022-01-12 20:30:36 +0100

    change to own repository

--- raspberry/install_display.sh
+++ raspberry/install_display.sh
@@ -1,5 +1,6 @@
 sudo rm -rf LCD-show
-git clone https://github.com/goodtft/LCD-show.git
+#git clone https://github.com/goodtft/LCD-show.git
+git clone https://github.com/woehrer12/LCD-show.git
 chmod -R 755 LCD-show
 cd LCD-show/
 sudo ./LCD35-show
