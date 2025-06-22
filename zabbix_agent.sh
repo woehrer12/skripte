cfd8e8301a6d47cfc0c80a3c274ad5e3e77283c8
Julian Wöhrer <julian.woehrer@gmail.com>
2025-06-22 13:01:02 +0200

    zabbix

---
+++ zabbix_agent.sh
@@ -0,0 +1,9 @@
+wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu24.04_all.deb
+sudo dpkg -i zabbix-release_latest_7.2+ubuntu24.04_all.deb
+sudo apt update
+sudo apt install zabbix-agent2 -y
+sudo apt install zabbix-agent2-plugin-mongodb zabbix-agent2-plugin-mssql zabbix-agent2-plugin-postgresql -y
+sudo systemctl restart zabbix-agent2
+sudo systemctl enable zabbix-agent2 
+
+sudo nano /etc/zabbix/zabbix_agent2.conf
