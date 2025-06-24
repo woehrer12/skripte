0e8bd3bbc8d18262e23d1ab6cc2af14a476e7d5a
woehrer12 <Julian.woehrer@gmail.com>
2025-06-24 11:00:37 +0200

    Update zabbix_agent.sh

--- zabbix_agent.sh
+++ zabbix_agent.sh
@@ -1,6 +1,7 @@
 wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu24.04_all.deb
 sudo dpkg -i zabbix-release_latest_7.2+ubuntu24.04_all.deb
 sudo apt update
+sudo apt install nano -y
 sudo apt install zabbix-agent2 -y
 sudo apt install zabbix-agent2-plugin-mongodb zabbix-agent2-plugin-mssql zabbix-agent2-plugin-postgresql -y
 sudo systemctl restart zabbix-agent2
